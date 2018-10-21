354894cf97f3b4edf9ec9deb0cfb17b9cbd886cd
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2018-10-21 08:41:15 -0500

    Added the functionality of being able to parse query strings from incoming requests, also can now parse headers.

--- index.js
+++ index.js
@@ -9,13 +9,19 @@ var server = http.createServer(function (req, res) {
     // Get the path from the URL.
     var path = parsedURL.pathname;
     var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+    // Get the query string as an object.
+    var queryStringObject = parsedURL.query;
     // Get the HTTP method.
     var method = req.method.toLowerCase();
+    // Get the headers as an object.
+    var headers = req.headers;
     // Send the response.
     res.end("Hello World from Server!\n");
     // Log the request path.
     console.log("Request received on path: ", trimmedPath);
     console.log("With HTTP method: ", method);
+    console.log("With these query string parameters: ", queryStringObject);
+    console.log("With these headers: ", headers);
 });
 // Start the server, and have it listen on port 3000.
 server.listen(3000, function () {
--- index.ts
+++ index.ts
@@ -11,14 +11,20 @@ const server = http.createServer((req, res) => {
     var parsedURL = url.parse(req.url, true);
     // Get the path from the URL.
     var path = parsedURL.pathname;
-    var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+    var trimmedPath: string = path.replace(/^\/+|\/+$/g, "");
+    // Get the query string as an object.
+    var queryStringObject: object = parsedURL.query;
     // Get the HTTP method.
     var method = req.method.toLowerCase();
+    // Get the headers as an object.
+    var headers = req.headers;
     // Send the response.
     res.end("Hello World from Server!\n");
     // Log the request path.
     console.log("Request received on path: ", trimmedPath);
     console.log("With HTTP method: ", method);
+    console.log("With these query string parameters: ", queryStringObject);
+    console.log("With these headers: ", headers);
 });
 
 // Start the server, and have it listen on port 3000.
