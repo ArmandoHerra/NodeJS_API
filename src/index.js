63e97717b3db65a6b47f0ec9d0c20d94a3915f58
Armando Herra <armandoherra369@gmail.com>
2019-06-01 23:07:25 -0500

    Payloads are now parsed and received.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 /*
  *
- * Index file.
+ *   Index file.
  *
  */
 
 // Dependencies.
 const http = require("http");
 const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 // The server should respond to all requests with a string.
 const server = http.createServer((req, res) => {
@@ -26,18 +27,31 @@ const server = http.createServer((req, res) => {
     // Get the headers as an object.
     const headers = req.headers;
 
-    // Send the response.
-    res.end("Hello World\n");
+    // Get the payload if any.
+    const decoder = new StringDecoder("utf-8");
+    let buffer = "";
+    req.on("data", data => {
+        buffer += decoder.write(data);
+    });
+    req.on("end", () => {
+        buffer += decoder.end();
+        // Send the response.
+        res.end("Hello World\n");
 
-    // Log the request path.
-    console.log(
-        `Request received on path: ${trimmedPath} with the method ${method}`
-    );
-    console.log(
-        "Request query string params: ",
-        JSON.stringify(queryStringObject)
-    );
-    console.log("Request headers: ", JSON.stringify(headers));
+        // Log the request path.
+        console.log(
+            `Request received on path: ${trimmedPath} with the method ${method}`
+        );
+        console.log(
+            "Request query string params: ",
+            JSON.stringify(queryStringObject)
+        );
+        console.log("Request headers: ", JSON.stringify(headers));
+        console.log(
+            "Request received with this payload: ",
+            JSON.stringify(buffer)
+        );
+    });
 });
 
 // Start the server, and have it listen on port 3000.
