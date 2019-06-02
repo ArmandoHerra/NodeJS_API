c0719828ac84218f00f0a42a0f3da25b112f894e
Armando Herra <armandoherra369@gmail.com>
2019-06-01 22:53:21 -0500

    Added the ability to parse and save the request method.

--- src/index.js
+++ src/index.js
@@ -16,11 +16,17 @@ const server = http.createServer((req, res) => {
     // Get the path.
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/\/\+|\/+$/g, "");
+
+    // Get the HTTP method.
+    const method = req.method.toLowerCase();
+
     // Send the response.
     res.end("Hello World\n");
 
     // Log the request path.
-    console.log("Request received on path: ", trimmedPath);
+    console.log(
+        `Request received on path: ${trimmedPath} with the method ${method}`
+    );
 });
 
 // Start the server, and have it listen on port 3000.
